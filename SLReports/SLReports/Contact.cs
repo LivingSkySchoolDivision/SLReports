006a34afb9a02c924fcf7c7ed3ba95d75379d941
Mark Strendin <mark.strendin@gmail.com>
2013-05-09 13:57:53 -0600

    added Contact class

---
+++ SLReports/SLReports/Contact.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SLReports
+{
+    public class Contact
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string studentNumber { get; set; }
+
+        public Contact(string firstName, string LastName, string StudentNumber)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.studentNumber = studentNumber;
+        }
+    }
+}
