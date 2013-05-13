1727dec315d4e6fff8b9b64765a76bfbd70a615c
Mark Strendin <mark.strendin@gmail.com>
2013-05-13 11:34:43 -0600

    Renamed student table so that the FIM table doesn't include all of the bloat that I need for this reporting tool.

--- SLReports/SLReports/ReportCard/index.aspx
+++ SLReports/SLReports/ReportCard/index.aspx
@@ -3,5 +3,18 @@
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="Main" runat="server">
     <h3>Report Card (Work In Progress)</h3>
-    
+    <br />
+    Report generated: <asp:Label ID="lblDate" runat="server" Text=""></asp:Label>
+    <br /><br />
+    <asp:Table ID="Table1" runat="server">
+        <asp:TableRow>
+            <asp:TableCell ColumnSpan="2"><asp:Label ID="lblStudentName" runat="server" Text=""></asp:Label></asp:TableCell>
+        </asp:TableRow>
+        <asp:TableRow>
+            <asp:TableCell Width="200">Home room</asp:TableCell>
+            <asp:TableCell>
+                <asp:Label ID="lblHomeRoom" runat="server" Text=""></asp:Label></asp:TableCell>
+        </asp:TableRow>
+    </asp:Table>
+
 </asp:Content>
--- SLReports/SLReports/ReportCard/index.aspx.cs
+++ SLReports/SLReports/ReportCard/index.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +10,30 @@ namespace SLReports.ReportCard
 {
     public partial class index : System.Web.UI.Page
     {
+        Student SelectedStudent;
+        String dbUser = @"sql_readonly";
+        String dbPassword = @"XTXVDUNHlrdbefjTBgY4";
+        String dbHost = "dcsql.lskysd.ca";
+        String dbDatabase = "SchoolLogicDB";
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            /* Load the current date into the date label field */
+            lblDate.Text = DateTime.Now.ToLongDateString() + " at " + DateTime.Now.ToShortTimeString();
 
+            /* Load a specific student, for testing */
+            String dbConnectionString = "data source=" + dbHost + ";initial catalog=" + dbDatabase + ";user id=" + dbUser + ";password=" + dbPassword + ";Trusted_Connection=false";
+
+            using (SqlConnection connection = new SqlConnection(dbConnectionString))
+            {
+                SelectedStudent = Student.loadThisStudent(connection, "12511");
+            }
+
+            if (SelectedStudent != null)
+            {
+                lblStudentName.Text = SelectedStudent.getDisplayName();
+                lblHomeRoom.Text = SelectedStudent.getHomeRoom();
+            }
         }
     }
 }
