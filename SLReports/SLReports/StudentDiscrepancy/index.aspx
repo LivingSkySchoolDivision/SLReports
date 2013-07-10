b54338f499192eefe04734a82e257a5adb83990f
Mark Strendin <mark.strendin@gmail.com>
2013-07-10 14:11:19 -0600

    Added a "report" to help diagnose why my student count is different than schoollogic's student count.

---
+++ SLReports/SLReports/StudentDiscrepancy/index.aspx
@@ -0,0 +1,50 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/BasicTemplate.Master" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="SLReports.StudentDiscrepancy.index" %>
+<asp:Content ID="Content2" ContentPlaceHolderID="Main" runat="server">
+    <h2>Student Count Discrepancy Calculator</h2>
+    <p>This tool can be used to help determine why the number of current students is different here compared to within school logic. I am not sure how they are detemining their list of current students, so this tool can compare a copy and pasted SchoolLogic report with the database, and tell you which students are missing. Hopefully this helps fine tune the database query, and provide more accurate numbers.</p>
+    <br />
+    <h2>Copy and paste student ID numbers here</h2>
+    <p>You should copy and paste the student ID column from an excel spreadsheet. This form expects <i>only</i> id numbers, each on a new line, no commas or semicolens.</p>
+    <form runat="server">
+        <asp:TextBox Height="300" TextMode="MultiLine" width="400" ID="txtInput" runat="server"></asp:TextBox><br />
+        <asp:Button ID="btnGo" runat="server" Text="Analyze" OnClick="btnGo_Click" />
+    </form>
+    <p>This may take some time - it can take over 25 million calculations.</p>
+    <br />
+    <asp:Label ID="lblStatus" runat="server" Text=""></asp:Label>
+
+
+    <asp:Table ID="tblResults" runat="server" Visible="false">
+        <asp:TableRow>
+            <asp:TableCell VerticalAlign="Top">
+                <h2>Missing from database <asp:Label ID="lblMissingFromDatabaseCount" runat="server" Text=""></asp:Label></h2>
+                <asp:Table ID="tblMissingFromDataSite" runat="server" CssClass="datatable" CellPadding="5">
+                    <asp:TableHeaderRow CssClass="datatable_header">
+                        <asp:TableCell Width="300">Given ID Number</asp:TableCell>
+                    </asp:TableHeaderRow>               
+                </asp:Table>                
+            </asp:TableCell>
+
+            <asp:TableCell VerticalAlign="Top">
+                <h2>Missing from input <asp:Label ID="lblMissingFromInputCount" runat="server" Text=""></asp:Label></h2>
+                <asp:Table ID="tblMissingFromInput" runat="server" CssClass="datatable" CellPadding="5">
+                    <asp:TableHeaderRow CssClass="datatable_header">
+                        <asp:TableCell Width="100">Student ID</asp:TableCell>
+                        <asp:TableCell Width="200">Student Name</asp:TableCell>
+                    </asp:TableHeaderRow>
+                </asp:Table>
+            </asp:TableCell>
+
+            <asp:TableCell VerticalAlign="Top">
+                <h2>Present in database <asp:Label ID="lblPresentCount" runat="server" Text=""></asp:Label></h2>
+                <asp:Table ID="tblMatchingStudents" runat="server" CssClass="datatable" CellPadding="5">
+                    <asp:TableHeaderRow CssClass="datatable_header">
+                        <asp:TableCell Width="100">Student ID</asp:TableCell>
+                        <asp:TableCell Width="200">Student Name</asp:TableCell>
+                    </asp:TableHeaderRow>
+                </asp:Table>
+            </asp:TableCell>
+        </asp:TableRow>
+    </asp:Table>
+    
+</asp:Content>
---
+++ SLReports/SLReports/StudentDiscrepancy/index.aspx.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SLReports.StudentDiscrepancy
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private TableRow addIDRow(int idnumber)
+        {
+            TableRow newRow = new TableRow();
+            TableCell newCell = new TableCell();
+            newCell.Text = idnumber.ToString();
+            newRow.Cells.Add(newCell);
+            return newRow;
+        }
+
+        private TableRow addStudentRow(Student student)
+        {
+            TableRow newRow = new TableRow();
+            TableCell newCell = new TableCell();
+            newCell.Text = student.getStudentID();
+            newRow.Cells.Add(newCell);
+
+            newCell = new TableCell();
+            newCell.Text = student.getDisplayName();
+            newRow.Cells.Add(newCell);
+
+            return newRow;
+        }
+
+        protected void btnGo_Click(object sender, EventArgs e)
+        {
+
+            string[] input = txtInput.Text.Split('\n');
+
+            List<int> inputtedIDNumbers = new List<int>();
+
+            foreach (string segment in input)
+            {
+                if (!string.IsNullOrEmpty(segment))
+                {
+                    inputtedIDNumbers.Add(int.Parse(segment));
+                }
+            }
+
+            lblStatus.Text = "Found " + inputtedIDNumbers.Count + " valid id numbers.";
+
+            /* Load all students */
+            List<Student> allStudents = new List<Student>(); 
+            String dbConnectionString = ConfigurationManager.ConnectionStrings["SchoolLogicDatabase"].ConnectionString;
+            using (SqlConnection connection = new SqlConnection(dbConnectionString))
+            {
+                allStudents = Student.loadAllStudents(connection);
+            }
+
+            List<int> idNumbersMissingFromDatabase = new List<int>();
+            List<Student> studentsMissingFromInput = new List<Student>();
+            List<Student> studentsFoundInDatabase = new List<Student>();
+            
+            foreach (Student student in allStudents)
+            {
+                bool foundMatch = false;
+                foreach (int testForThisID in inputtedIDNumbers)
+                {
+                    if (testForThisID == int.Parse(student.getStudentID()))
+                    {
+                        foundMatch = true;
+                    }
+                }
+
+                if (foundMatch)
+                {
+                    studentsFoundInDatabase.Add(student);
+                }
+                else
+                {
+                    studentsMissingFromInput.Add(student);
+                }
+            }
+
+            foreach (int testForThisID in inputtedIDNumbers)
+            {
+                bool foundMatch = false;
+                foreach (Student student in allStudents)
+                {
+                    if (testForThisID == int.Parse(student.getStudentID()))
+                    {
+                        foundMatch = true;
+                    }
+                }
+
+                if (!foundMatch)
+                {
+                    idNumbersMissingFromDatabase.Add(testForThisID);
+                }
+            }
+
+            foreach (int id in idNumbersMissingFromDatabase)
+            {
+                tblMissingFromDataSite.Rows.Add(addIDRow(id));
+            }
+
+            foreach (Student student in studentsMissingFromInput)
+            {
+                tblMissingFromInput.Rows.Add(addStudentRow(student));   
+            }
+
+            foreach (Student student in studentsFoundInDatabase)
+            {
+                tblMatchingStudents.Rows.Add(addStudentRow(student));
+            }
+
+            lblMissingFromDatabaseCount.Text = "(" + idNumbersMissingFromDatabase.Count + ")";
+            lblMissingFromInputCount.Text = "(" + studentsMissingFromInput.Count + ")";
+            lblPresentCount.Text = "(" + studentsFoundInDatabase.Count + ")";
+
+            if (
+                (idNumbersMissingFromDatabase.Count > 0) ||
+                (studentsMissingFromInput.Count > 0) ||
+                (studentsFoundInDatabase.Count > 0)
+                )
+            {
+                tblResults.Visible = true;
+            }
+            else
+            {
+                tblResults.Visible = false;
+            }
+
+
+        }
+    }
+}
