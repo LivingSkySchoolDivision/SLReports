b3ed51f9799b92838cf73df51f47fabd6e7e5f27
Mark Strendin <mark.strendin@gmail.com>
2013-07-25 12:18:32 -0600

    Added a duplicate student finder page to display potential duplicate students in the system

---
+++ SLReports/SLReports/Duplicates/index.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/BasicTemplate.Master" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="SLReports.Duplicates.index" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="Main" runat="server">
+    <h2>Duplicate Government IDs <asp:Label ID="lblIdsCount" runat="server" Text=""></asp:Label></h2>
+    <asp:Table ID="tblGovIDs" runat="server" CssClass="datatable" CellPadding="5">
+        <asp:TableHeaderRow CssClass="datatable_header">
+            <asp:TableHeaderCell>Student Number</asp:TableHeaderCell>
+            <asp:TableHeaderCell>Student Name</asp:TableHeaderCell>
+            <asp:TableHeaderCell>Government ID</asp:TableHeaderCell>
+            <asp:TableHeaderCell>Gender</asp:TableHeaderCell>
+            <asp:TableHeaderCell>Date of Birth</asp:TableHeaderCell>
+            <asp:TableHeaderCell>School</asp:TableHeaderCell>
+        </asp:TableHeaderRow>
+    </asp:Table>
+
+    <h2>Duplicate Names <asp:Label ID="lblNamesCount" runat="server" Text=""></asp:Label></h2>
+    <asp:Table ID="tblNames" runat="server" CssClass="datatable" CellPadding="5">
+        <asp:TableHeaderRow CssClass="datatable_header">
+            <asp:TableHeaderCell>Student Number</asp:TableHeaderCell>
+            <asp:TableHeaderCell>Student Name</asp:TableHeaderCell>
+            <asp:TableHeaderCell>Government ID</asp:TableHeaderCell>
+            <asp:TableHeaderCell>Gender</asp:TableHeaderCell>
+            <asp:TableHeaderCell>Date of Birth</asp:TableHeaderCell>
+            <asp:TableHeaderCell>School</asp:TableHeaderCell>
+        </asp:TableHeaderRow>
+    </asp:Table>
+</asp:Content>
---
+++ SLReports/SLReports/Duplicates/index.aspx.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SLReports.Duplicates
+{
+    public partial class index : System.Web.UI.Page
+    {
+        private TableRow addStudentRow(Student student)
+        {
+            TableRow newRow = new TableRow();
+
+            TableCell newCell = null;
+
+            newCell = new TableCell();
+            newCell.Text = student.getStudentID();
+            newRow.Cells.Add(newCell);
+
+            newCell = new TableCell();
+            newCell.Text = student.getDisplayName();
+            newRow.Cells.Add(newCell);
+
+            newCell = new TableCell();
+            newCell.Text = student.getGovernmentID();
+            newRow.Cells.Add(newCell);
+
+            newCell = new TableCell();
+            newCell.Text = student.getGender();
+            newRow.Cells.Add(newCell);
+
+            newCell = new TableCell();
+            newCell.Text = student.getDateOfBirth().ToShortDateString();
+            newRow.Cells.Add(newCell);
+
+            newCell = new TableCell();
+            newCell.Text = student.getSchoolName();
+            newRow.Cells.Add(newCell);          
+
+
+            return newRow;
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            List<Student> allStudents = new List<Student>();
+
+            String dbConnectionString = ConfigurationManager.ConnectionStrings["SchoolLogicDatabase"].ConnectionString;
+            using (SqlConnection connection = new SqlConnection(dbConnectionString))
+            {
+                allStudents = Student.loadAllStudents(connection);
+            }
+
+
+            Dictionary<string, Student> workspace_govid = new Dictionary<string, Student>();
+            Dictionary<string, Student> workspace_names = new Dictionary<string, Student>();
+            List<Student> foundDuplicates_govid = new List<Student>();
+            List<Student> foundDuplicates_names = new List<Student>();
+
+            foreach (Student student in allStudents)
+            {
+                if (!string.IsNullOrEmpty(student.getGovernmentID()))
+                {
+                    if (!workspace_govid.ContainsKey(student.getGovernmentID()))
+                    {
+                        workspace_govid.Add(student.getGovernmentID(), student);
+                    }
+                    else
+                    {
+                        foundDuplicates_govid.Add(student);
+                        foundDuplicates_govid.Add(workspace_govid[student.getGovernmentID()]);
+                    }
+                }
+
+                String condensedName = student.getDisplayName().ToLower().Replace(" ",string.Empty).Replace("-", string.Empty);
+                if (!workspace_names.ContainsKey(condensedName))
+                {
+                    workspace_names.Add(condensedName, student);
+                }
+                else
+                {
+                    foundDuplicates_names.Add(student);
+                    foundDuplicates_names.Add(workspace_names[condensedName]);
+                }
+            }
+
+            foreach (Student student in foundDuplicates_govid)
+            {
+                tblGovIDs.Rows.Add(addStudentRow(student));
+            }
+            lblIdsCount.Text = "(" + foundDuplicates_govid.Count.ToString() + ")";
+
+            foreach (Student student in foundDuplicates_names)
+            {
+                tblNames.Rows.Add(addStudentRow(student));
+            }
+            lblNamesCount.Text = "(" + foundDuplicates_names.Count.ToString() + ")";
+
+
+        }
+    }
+}
