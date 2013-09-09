38f88778c36fd7fcdce593cafd9e9767aabeb52c
Mark Strendin <mark.strendin@gmail.com>
2013-09-09 17:28:14 -0600

    Added a page that indicates who has access to the site, to remain as transparent as possible

---
+++ SLReports/SLReports/Logs/WhoHasAccess.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/BasicTemplate.Master" AutoEventWireup="true" CodeBehind="WhoHasAccess.aspx.cs" Inherits="SLReports.Logs.WhoHasAccess" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="Main" runat="server">
+    <div style="width: 600px; margin-left: auto; margin-right: auto;">
+    <h2>Who has access to this website</h2>
+
+    <p>
+        The users listed below are able to access this website.
+    </p>
+
+    <p>
+        To add a user to the access list, please contact the <a href="https://helpdesk.lskysd.ca">Technology Help Desk</a>, or a member of the tech department.
+    </p>
+
+
+    <asp:Table ID="Table1" runat="server" Width="500" HorizontalAlign="Center">
+        <asp:TableRow>
+            <asp:TableCell VerticalAlign="Top">
+                <h3>Site administrators</h3>
+                <asp:Table ID="tblAdministrators" runat="server">
+                </asp:Table>
+
+            </asp:TableCell>
+            <asp:TableCell VerticalAlign="Top">
+                <h3>Site users</h3>
+                <asp:Table ID="tblUsers" runat="server">
+                </asp:Table>
+
+            </asp:TableCell>
+        </asp:TableRow>
+    </asp:Table>
+    </div>
+    
+
+    
+
+</asp:Content>
---
+++ SLReports/SLReports/Logs/WhoHasAccess.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SLReports.Logs
+{
+    public partial class WhoHasAccess : System.Web.UI.Page
+    {     
+        TableRow addUserRow(string username)
+        {
+            TableRow newRow = new TableRow();
+
+            TableCell usernameCell = new TableCell();
+            usernameCell.Text = username;
+            newRow.Cells.Add(usernameCell);
+
+
+            return newRow;
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            //lblAdminGroup.Text = LSKYCommon.adminGroupName;
+            //lblUserGroup.Text = LSKYCommon.userGroupName;
+
+            // Get a list of users from security groups - recursively
+            List<string> groupMembers = LSKYCommon.getGroupMembers("lskysd", LSKYCommon.userGroupName);
+            List<string> adminGroupMembers = LSKYCommon.getGroupMembers("lskysd", LSKYCommon.adminGroupName);
+
+            groupMembers.Sort();
+            adminGroupMembers.Sort();
+
+            foreach (string user in adminGroupMembers)
+            {
+                tblAdministrators.Rows.Add(addUserRow(user));
+            }
+            
+            foreach (string user in groupMembers)
+            {
+                tblUsers.Rows.Add(addUserRow(user));
+            }
+            
+
+        }
+    }
+}
